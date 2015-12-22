35fdfd4fd1f2e90b5de5658301c3a75fb4af622b
Ganis Z S <ganiszul.fa@gmail.com>
2015-12-22 17:00:31 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ cp ~/.tmux.conf ~/.tmux_backup.conf
 cp ~/.tmux/.tmux.conf ~/.tmux.conf
 cd ~/.tmux
 git clone https://github.com/tmux-plugins/tmux-resurrect.git 
+echo "alias tmux='tmux -2'" > ~/.bashrc
+echo "now run source ~/.bashrc"
