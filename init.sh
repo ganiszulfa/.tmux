9bf2dbb759b793d7404ff0def4522910974f25bd
Ganis Z S <ganiszul.fa@gmail.com>
2018-10-16 12:31:55 +0700

    change dir color

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ cp ~/.tmux/.tmux.conf ~/.tmux.conf
 cd ~/.tmux
 git clone https://github.com/tmux-plugins/tmux-resurrect.git 
 echo "alias tmux='tmux -2'" >> ~/.bashrc
+echo "LS_COLORS=$LS_COLORS:'di=0;35:' ; export LS_COLORS" >> ~/.bashrc
 echo "now run source ~/.bashrc"
